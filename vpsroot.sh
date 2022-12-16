6e399e13fc712669bc4809030bf2835c713eaffd
cr4r <cr4rrr@gmail.com>
2022-12-16 22:22:02 +0700

    add projek dari fisabiliyusri

--- vpsroot.sh
+++ vpsroot.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
+. config
 # Mod By SL
 #echo "$crot    ALL=(ALL:ALL) ALL" >> /etc/sudoers;
-wget -qO- -O /etc/ssh/sshd_config https://raw.githubusercontent.com/fisabiliyusri/Mantap/main/sshd_config;
-systemctl restart sshd;
-clear;
-echo -e "Masukkan Password:";
-read -e pwe;
-usermod -p `perl -e "print crypt("$pwe","Q4")"` root;
-clear;
+wget -qO- -O /etc/ssh/sshd_config https://raw.githubusercontent.com/cr4r/ServerVPN/main/sshd_config
+systemctl restart sshd
+clear
+
+while [[ ${pwe} = "" ]]; do
+  msg -org "Masukkan Password: " && read pwe
+  tput cuu1 && tput dl1
+done
+
+usermod -p $(perl -e "print crypt("$pwe","Q4")") root
+clear
+
 printf "Mohon Simpan Informasi Akun VPS Ini
 ============================================
 Akun Root (Akun Utama)
-Ip address = $(curl -Ls http://ipinfo.io/ip)
+Ip address = $(IP)
 Username   = root
 Password   = $pwe
-============================================
-echo "";
-exit;
+============================================"
+read enter && exit
