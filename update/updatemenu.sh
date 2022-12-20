0aad27b432f006d8d31c61b31193c7bca0bab57d
cr4r <cr4rrr@gmail.com>
2022-12-20 16:06:13 +0700

    d

--- update/updatemenu.sh
+++ update/updatemenu.sh
@@ -1,45 +1,7 @@
 #!/bin/bash
 # ==========================================
+. <(curl -s https://raw.githubusercontent.com/cr4r/ServerVPN/main/config)
 
-msg() {
-  local colors="/etc/new-cr4r-color"
-  if [[ ! -e $colors ]]; then
-    COLOR[0]='\033[1;37m' #BRAN='\033[1;37m'
-    COLOR[1]='\e[31m'     #VERMELHO='\e[31m'
-    COLOR[2]='\e[32m'     #VERDE='\e[32m'
-    COLOR[3]='\e[33m'     #AMARELO='\e[33m'
-    COLOR[4]='\e[34m'     #AZUL='\e[34m'
-    COLOR[5]='\e[35m'     #MAGENTA='\e[35m'
-    COLOR[6]='\033[1;36m' #MAG='\033[1;36m'
-  else
-    local COL=0
-    for number in $(cat $colors); do
-      case $number in
-      1) COLOR[$COL]='\033[1;37m' ;;
-      2) COLOR[$COL]='\e[31m' ;;
-      3) COLOR[$COL]='\e[32m' ;;
-      4) COLOR[$COL]='\e[33m' ;;
-      5) COLOR[$COL]='\e[34m' ;;
-      6) COLOR[$COL]='\e[35m' ;;
-      7) COLOR[$COL]='\033[1;36m' ;;
-      esac
-      let COL++
-    done
-  fi
-  NEGRITO='\e[1m'
-  SEMCOR='\e[0m'
-  cor="${COLOR[4]}————————————————————"
-  garis=${SEMCOR}${cor}${SEMCOR}
-  case $1 in
-  -ne) cor="${COLOR[1]}${NEGRITO}" && echo -ne "${cor}${2}${SEMCOR}" ;;
-  -org) cor="${COLOR[3]}${NEGRITO}" && echo -e "${cor}${2}${SEMCOR}" ;;
-  -warn) cor="${COLOR[3]}${NEGRITO}[!] ${COLOR[1]}" && echo -e "${cor}${2}${SEMCOR}" ;;
-  -red) cor="${COLOR[1]}${NEGRITO}" && echo -e "${cor}${2}${SEMCOR}" ;;
-  -gr) cor="${COLOR[6]}${NEGRITO}" && echo -e "${cor}${2}${SEMCOR}" ;;
-  -bra) cor="${COLOR[0]}${NEGRITO}" && echo -e "${cor}${2}${SEMCOR}" ;;
-  -line) echo -e "${garis}${2}${garis}" ;;
-  esac
-}
 # hapus menu
 rm -rf menu
 rm -rf ipsaya
@@ -72,16 +34,23 @@ rm -rf install-sldns
 rm -rf addssh
 
 loktemp=$(mktemp -d)
-for abc in $(curl -s ${rawRepo}/update/config); do
+mkdir -p ${loktemp}
+for abc in $(curl -Ls ${rawRepo}update/config); do
+  # echo $abc
   tol=${rawRepo}${abc}
-  wget -O $loktemp $tol
+  echo $tol
+  URL_NOPRO=${tol:7}
+  URL_REL=${URL_NOPRO#*/}
+  namaFile="/${URL_REL%%\?*}"
+  # echo $namaFile
+  wget -O "${loktemp}/${namaFile}" ${tol} &>/dev/null
 done
 
-chmod +x $loktemp/*
-mv $loktemp/* /usr/bin/
+# chmod +x $loktemp/*
+# mv $loktemp/* /usr/bin/
 
-sl-download-info
-#install-sldns
-#install-ss-plugin
-#xray-grpc
-cd $home
+# sl-download-info
+# #install-sldns
+# #install-ss-plugin
+# #xray-grpc
+# cd $home
